532257012ae56151a71f09117f388c7db0d1bd3d
3jm <faze.frantacle@gmail.com>
2023-08-09 00:26:52 -0400

    First commit

---
+++ app/about/page.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Wrld | About',
+  description: 'About wrld and it\'s intensions.',
+}
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <div className="text-center">
+        <h1 className="text-4xl mb-2">About Wrld</h1>
+        <h3 className="text-center block">this page will be about wrld if you can't tell</h3>
+      </div>
+    </main>
+  );
+}
---
+++ app/profile/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <div className="text-center">
+        <h1 className="text-4xl mb-2">Profile</h1>
+        <h3 className="text-center block">this page will be your profile for wrld</h3>
+      </div>
+    </main>
+  );
+}
