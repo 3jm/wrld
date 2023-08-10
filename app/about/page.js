426249d048714fc2c95a82fb0b9658cb2c893781
3jm <faze.frantacle@gmail.com>
2023-08-10 03:35:47 -0400

    added and fixed stuff

--- app/about/page.js
+++ app/about/page.js
@@ -14,15 +14,15 @@ export default function Home() {
            alt="Logo"
          />
        </Link>
-        <h1 className="text-center text-7xl mb-2 font-sans font-bold">About <wrld className="text-red-500	">Wrld</wrld></h1>
+        <h1 className="text-center text-7xl mb-2 font-sans font-bold">About <wrld className="text-[#b899ff]	">Wrld</wrld></h1>
         <div className="w-5/6 text-center">
-          <h3 className="text-center block break-normal"></h3><wrld className="text-red-500	">Wrld </wrld>is a bot that I designed as a small project to consume some of my freetime as I was getting bored with things to do. Once I started building the basic features of the bot I decided to show it off to Pepiti & others in the MX Bikes Community Discord server. This marks <wrld className="text-red-500	">Wrld </wrld>'s birthday! 06/29/2023. Almost immediately some people thought the bot was cool, and I thought it was nice that others thought it was a cool thing so I continued. About 2 weeks later I posted some more pictures & videos of the bot and it gained more attention by others and sure enough, 07/12/2023, <wrld className="text-red-500	">Wrld </wrld> had made it into the official MX Bikes Community Discord server!
+          <h3 className="text-center block break-normal"></h3><wrld className="text-[#b899ff]	">Wrld </wrld>is a bot that I designed as a small project to consume some of my freetime as I was getting bored with things to do. Once I started building the basic features of the bot I decided to show it off to Pepiti & others in the MX Bikes Community Discord server. This marks <wrld className="text-[#b899ff]	">Wrld </wrld>'s birthday! 06/29/2023. Almost immediately some people thought the bot was cool, and I thought it was nice that others thought it was a cool thing so I continued. About 2 weeks later I posted some more pictures & videos of the bot and it gained more attention by others and sure enough, 07/12/2023, <wrld className="text-[#b899ff]	">Wrld </wrld> had made it into the official MX Bikes Community Discord server!
           <br /><br />
           <div className='flex justify-between flex-col items-center '>
             <hr className='w-64'/>
           </div>
           <br />
-          <h3 className="text-center block break-normal">As of now <wrld className="text-red-500	">Wrld </wrld> is slowly growing, serving around <wrld className="text-red-500	">20+</wrld> servers and <wrld className="text-red-500	">26k+ </wrld> users!</h3>
+          <h3 className="text-center block break-normal">As of now <wrld className="text-[#b899ff]	">Wrld </wrld> is slowly growing, serving around <wrld className="text-[#b899ff]	">20+</wrld> servers and <wrld className="text-[#b899ff]	">26k+ </wrld> users!</h3>
         </div>
       </div>
     </main>
--- app/layout.js
+++ app/layout.js
@@ -16,36 +16,55 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className={styles.pageContainer}>
-          <nav className={styles.navBar}>
-            <ul className={styles.navList}>
-              <li className={styles.logoItem}>
-                <Link href="/">
-                  <img
-                    src="https://cdn.discordapp.com/avatars/1123436273756033074/dd4f4dbc252c76b0e25db45557e7a016.png?size=1024"
-                    alt="Logo"
-                    className={styles.logoImage}
-                  />
-                </Link>
-              </li>
-              <ul className={styles.navList}>
-                <li className={styles.navItem}>
-                  <Link href="/">Home</Link>
-                </li>
-                <li className={styles.navItem}>
-                  <Link href="/about">About</Link>
-                </li>
-                <li className={styles.navItem}>
-                  <Link href="/profile">Profile</Link>
-                </li>
-              </ul>
-            </ul>
-          </nav>
+        <nav className="flex justify-between items-center py-4 px-6 bg-zinc-950">
+          <div className="flex items-center">
+            <Link href="/">
+              <img
+                src="https://cdn.discordapp.com/avatars/1123436273756033074/dd4f4dbc252c76b0e25db45557e7a016.png?size=1024"
+                alt="Logo"
+                className="h-12 w-auto mr-4"
+              />
+            </Link>
+          </div>
+          <ul className="flex space-x-8 mr-4 text-lg">
+            <li>
+              <Link href="/" className="text-white hover:text-zinc-300 transition">Home</Link>
+            </li>
+            <li>
+              <Link href="/about" className="text-white hover:text-zinc-300 transition">About</Link>
+            </li>
+            <li>
+              <Link href="/profile" className="text-white hover:text-zinc-300 transition">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+
           <main className={styles.mainContent}>
             {children}
           </main>
-          <footer className={styles.footer}>
-            <p>&copy; 2023 Wrld. All rights reserved.</p>06/29/2023 
+          <footer className='py-5 bg-[#101010] text-center flex flex-wrap justify-between'>
+            <div className="w-full md:w-1/3 p-4 flex items-center">
+              <img src="/assets/logo.png" alt="Logo" className="mr-6 ml-12" style={{ width: '100px', height: 'auto' }} />
+              <div className="text-left">
+                <p className="font-bold text-white">Wrld</p>
+                <p className="text-zinc-500">06/29/2023</p>
+                <p className="text-zinc-500">Owned by <span className='text-[#b899ff]'>Blind</span></p>
+                <p className="text-zinc-500">@ 2023 <span className='text-[#b899ff]'>Wrld</span>. All rights reserved.</p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/3 p-4">
+              <p className="font-bold text-white">Contact</p>
+              <p className="text-zinc-500">Discord: <span className='text-[#b899ff]'>@onlyblind</span></p>
+              <p className="text-zinc-500">Server: <a href='https://discord.gg/2vEqW5GfHU' className="underline" target="_blank" rel="noopener noreferrer">discord.gg/2vEqW5GfHU</a></p>
+            </div>
+            <div className="w-full md:w-1/3 p-4">
+              <p className="font-bold text-white">Links</p>
+              <p className="text-zinc-500">
+                <a href="https://pepiti.com/" className="underline" target="_blank" rel="noopener noreferrer">https://pepiti.com/</a>
+              </p>
+            </div>
           </footer>
+
         </div>
       </body>
     </html>
