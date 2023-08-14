6c982ab361ed1bd2dfa6e6de52883905da58b3f1
3jm <faze.frantacle@gmail.com>
2023-08-13 22:31:51 -0400

    added and fixed stuff

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import styles from './layout.module.css'
 import React from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Wrld | MXB Bot',
@@ -14,13 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <div className={styles.pageContainer}>
         <nav className="flex justify-between items-center py-4 px-6 bg-zinc-950">
           <div className="flex items-center">
             <Link href="/">
               <img
-                src="https://cdn.discordapp.com/avatars/1123436273756033074/dd4f4dbc252c76b0e25db45557e7a016.png?size=1024"
+                src="/assets/logo.png"
                 alt="Logo"
                 className="h-12 w-auto mr-4"
               />
@@ -39,10 +37,12 @@ export default function RootLayout({ children }) {
           </ul>
         </nav>
 
-          <main className={styles.mainContent}>
-            {children}
-          </main>
-          <footer className='py-5 bg-[#101010] text-center flex flex-wrap justify-between'>
+        <main className={styles.mainContent}>
+          {children}
+        </main>
+          
+        <footer className='py-5 bg-[#101010] text-center'>
+          <div className="flex flex-wrap justify-between md:mb-0 mb-4"> {/* Added margin for mobile */}
             <div className="w-full md:w-1/3 p-4 flex items-center">
               <img src="/assets/logo.png" alt="Logo" className="mr-6 ml-12" style={{ width: '100px', height: 'auto' }} />
               <div className="text-left">
@@ -55,16 +55,18 @@ export default function RootLayout({ children }) {
             <div className="w-full md:w-1/3 p-4">
               <p className="font-bold text-white">Contact</p>
               <p className="text-zinc-500">Discord: <span className='text-[#b899ff]'>@onlyblind</span></p>
-              <p className="text-zinc-500">Server: <a href='https://discord.gg/2vEqW5GfHU' className="underline" target="_blank" rel="noopener noreferrer">discord.gg/2vEqW5GfHU</a></p>
+              <p className="text-zinc-500 ">Server: <a href='https://discord.gg/2vEqW5GfHU' className="underline hover:text-[#8652ff] transition duration-300" target="_blank" rel="noopener noreferrer">discord.gg/2vEqW5GfHU</a></p>
             </div>
             <div className="w-full md:w-1/3 p-4">
               <p className="font-bold text-white">Links</p>
               <p className="text-zinc-500">
-                <a href="https://pepiti.com/" className="underline" target="_blank" rel="noopener noreferrer">https://pepiti.com/</a>
+                <a href="https://pepiti.com/" className="underline hover:text-[#8652ff] transition duration-300" target="_blank" rel="noopener noreferrer">https://pepiti.com/</a>
+                <br />
+                <a href="https://mymxb.com/" className="underline hover:text-[#8652ff] transition duration-300" target="_blank" rel="noopener noreferrer">https://MyMXB.com/</a>
               </p>
             </div>
-          </footer>
-
+          </div>
+        </footer>
         </div>
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,58 @@
+import styles from './layout.module.css';
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <div className="text-center">
-        <h1 className="text-4xl mb-2">Welcome to the shit show :D</h1>
-        <small className="text-center block">I have no idea what I'm doing 😰</small>
-        <small className="text-center block">Next is so confusing</small>
+    <main className="flex flex-col justify-center items-center p-4 md:p-12">
+      <div className="flex flex-col md:flex-row w-full max-w-screen-xl">
+        <div className="w-full md:w-1/2 p-4 md:h-[70vh] flex justify-center items-center">
+          <img src="/assets/logo.png" alt="Logo" className="w-3/4 h-auto md:w-1/2 md:h-auto" />
+        </div>
+        <div className="w-full md:w-1/2 p-4 md:h-[70vh] flex flex-col justify-center items-center">
+          <h1 className="text-3xl font-semibold mb-4 text-[#b899ff]">Wrld</h1>
+          <p className="text-lg text-center">
+            Enhance your Discord server experience with Wrld Bot. From MXB commands to Fun commands, Wrld has it all. With a sleek design and powerful functions, Wrld is the perfect MXB Companion bot for your server.
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center mt-8">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className={`w-6 h-6 ${styles["jump-arrow"]}`}>
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+        </svg>
+      </div>
+      <div>
+        <div className="flex flex-col-reverse md:flex-row w-full max-w-screen-xl mt-8 text-center">
+          <div className="w-full md:w-1/2 p-4 md:h-[40vh] flex flex-col justify-center items-center">
+            <img src="https://cdn-icons-png.flaticon.com/512/1202/1202760.png" alt="Image 1" className="w-3/4 h-auto md:w-1/2 md:h-auto" />
+          </div>
+          <div className="w-full md:w-1/2 p-4 md:h-[40vh] flex flex-col justify-center items-center">
+            <h2 className="text-2xl font-semibold mb-2 text-[#b899ff]">Statistics</h2>
+            <p className="text-lg text-gray-300 text-center">
+              We currently serve 20+ Servers with around 25k+ members total!
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row w-full max-w-screen-xl mt-8">
+          <div className="w-full md:w-1/2 p-4 md:h-[40vh] flex flex-col justify-center items-center">
+              <h2 className="text-2xl font-semibold mb-2 text-[#b899ff]">Commands</h2>
+              <p className="text-lg text-gray-300 text-center">
+                Wrld has around 15+ commands dedicated to MX Bikes itself, and another about 10 for misc
+              </p>
+            </div>
+            <div className="w-full md:w-1/2 p-4 md:h-[40vh] flex flex-col justify-center items-center">
+              <img src="https://freepngimg.com/thumb/symbol/63241-console-command-line-icons-terminal-computer-linux-interface.png" alt="Image 2" className="w-3/4 h-auto md:w-1/2 md:h-auto" />
+          </div>
+        </div>
+        <div className="flex flex-col-reverse md:flex-row w-full max-w-screen-xl mt-8">
+          <div className="w-full md:w-1/2 p-4 md:h-[40vh] flex justify-center items-center">
+            <img src="https://cdn-icons-png.flaticon.com/512/5269/5269970.png" alt="Image 3" className="w-3/4 h-auto md:w-1/2 md:h-auto" />
+          </div>
+          <div className="w-full md:w-1/2 p-4 md:h-[40vh] flex flex-col justify-center items-center">
+            <h2 className="text-2xl font-semibold mb-2 text-[#b899ff]">Need Help?</h2>
+            <p className="text-lg text-gray-300 text-center">
+              Join our discord and ask your questions that you need in order for the bot to be more fun and useful to you and your community!
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
