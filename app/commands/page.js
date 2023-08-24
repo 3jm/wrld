39c21cdb262417836d43a733bbd411500860aad6
3jm <faze.frantacle@gmail.com>
2023-08-24 19:10:04 -0400

    added and fixed stuff

--- app/commands/page.js
+++ app/commands/page.js
@@ -6,125 +6,125 @@ import styles from '../layout.module.css';
 const commands = [
   {
     name: "/help",
-    description: "Redirects you here or brings up the select menu through a command."
+    description: "Redirects you here or brings up the select menu through a command.",
   },
   {
     name: "/search stats <guid>",
-    description: "Returns stats on a user if valid guid."
+    description: "Returns stats on a user if valid guid.",
   },
   {
     name: "/search records <guid>",
-    description: "Returns personal records on a user if valid guid."
+    description: "Returns personal records on a user if valid guid.",
   },
   {
     name: "/search leaderstats",
-    description: "Returns top 3 people for every category on pepiti servers."
+    description: "Returns top 3 people for every category on pepiti servers.",
   },
   {
     name: "/search races <guid>",
-    description: "Returns a list of a users races based on guid."
+    description: "Returns a list of a users races based on guid.",
   },
   {
     name: "/search race <id>",
-    description: "Returns information on a race if valid race id."
+    description: "Returns information on a race if valid race id.",
   },
   {
     name: "/search username <username/guid>",
-    description: "Search for a user based on what is provided."
+    description: "Search for a user based on what is provided.",
   },
   {
     name: "/whois <guid>",
-    description: "Returns a users Steam & more information."
+    description: "Returns a users Steam & more information.",
   },
   {
     name: "/search blacklist <guid>",
-    description: "Searches the global blacklist and checks if the guid provided is blacklisted."
+    description: "Searches the global blacklist and checks if the guid provided is blacklisted.",
   },
   {
-    name: "/setup <guid> <channel>",
-    description: "This sets up automatic rider statistics from pepiti servers, this updates every hour."
+    name: "/setup autostats <guid> <channel>",
+    description: "This sets up automatic rider statistics from pepiti servers, this updates every hour.",
+    permissions: ["Manage Guild"]
   },
   {
     name: "/cancel <guid>",
-    description: "Cancels automatic rider statistics for that guid. (NEED manage_messages PERMS)."
+    description: "Cancels automatic rider statistics for that guid. (NEED manage_messages PERMS).",
+    permissions: ["Manage Messages"]
   },
   {
     name: "/autostatsview",
-    description: "Returns a list of automatic stats setup in your server."
+    description: "Returns a list of automatic stats setup in your server.",
   },
   {
     name: "/embed <code>",
-    description: "Send a custom embed using generated code from https://www.bazhar.xyz/embed"
+    description: "Send a custom embed using generated code from https://3jm.github.io/wrld-embed-generator/, requires permissions below if sending to any channel that is not the one the command is ran in.",
+    permissions: ["Manage Channels"]
   },
   {
     name: "/information info",
-    description: "Basic info about the bot."
+    description: "Basic info about the bot.",
   },
   {
     name: "/information ping",
-    description: "Bot's latency."
+    description: "Bot's latency.",
   },
   {
     name: "/information github <username>",
-    description: "View someones github."
+    description: "View someones github.",
   },
   {
     name: "/duck",
-    description: "Returns a cute duck."
+    description: "Returns a cute duck.",
   },
   {
     name: "/names lookup <user: optional>",
-    description: "Shows past usernames for a user."
+    description: "Shows past usernames for a user.",
   },
   {
     name: "/names clear",
-    description: "Clears YOUR names from the database."
+    description: "Clears YOUR names from the database.",
   },
   {
     name: "/information about",
-    description: "Show's information about the server."
+    description: "Show's information about the server.",
   },
   {
     name: "/mymxb races",
-    description: "View a list of upcoming races."
+    description: "View a list of upcoming races.",
   },
   {
     name: "/mymxb championship",
-    description: "View a list of championship series races."
+    description: "View a list of championship series races.",
   },
   {
     name: "/mymxb statistics",
-    description: "View a racer's statistics."
+    description: "View a racer's statistics.",
   },
   {
     name: "/mymxb registered",
-    description: "Check if a user is registered for a specific race."
+    description: "Check if a user is registered for a specific race.",
   },
   {
     name: "/mymxb results",
-    description: "View the results a user got in a specific race."
+    description: "View the results a user got in a specific race.",
   },
   {
     name: "/settings commands restrict <channel>",
-    description: "Set a commands channel for the bot (Multiple channels allowed)."
+    description: "Set a commands channel for the bot (Multiple channels allowed).",
+    permissions: ["Manage Guild"]
   },
   {
     name: "/settings commands unrestrict",
-    description: "Remove command channel restrictions."
+    description: "Remove command channel restrictions.",
+    permissions: ["Manage Guild"]
   },
   {
     name: "/changelog",
-    description: "View the most recent or all MX Bikes game updates with patch notes."
+    description: "View the most recent or all MX Bikes game updates with patch notes.",
   },
 ];
 
 export default function Home() {
-  const [activeCommand, setActiveCommand] = useState(null);
   const [searchQuery, setSearchQuery] = useState('');
-
-  const handleButtonClick = (index) => {
-    setActiveCommand(index);
-  };
 
   const numCommands = commands.length;
 
@@ -132,8 +132,37 @@ export default function Home() {
     command.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  const PermissionTag = ({ permission }) => (
+    <span className="inline-block bg-[#303030] rounded-full px-2 py-1 text-sm font-semibold text-white mr-2 mb-2">
+      {permission}
+    </span>
+  );
+  
+  
+  const CommandCard = ({ command }) => (
+    <div className="command-card-wrapper">
+      <div className="command-card">
+        <div className="command-header flex justify-between items-center">
+          <div className="command-name text-[#8652ff] font-bold">{command.name}</div>
+        </div>
+        <div className="command-description text-white mt-2">{command.description}</div>
+        {command.permissions && command.permissions.length > 0 && (
+          <div className="command-permissions mt-2">
+            <div className="text-[#7d7d7d]">Permissions:</div>
+            <div className="mt-2">
+              {command.permissions.map((permission, index) => (
+                <PermissionTag key={index} permission={permission} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+  
+  
+
   return (
-    <client>
       <main className="flex justify-center items-center p-4 md:p-12">
         <div className="flex flex-col md:flex-row w-full md:w-3/4 justify-center items-start">
           <div className="bg-[#101010] p-4 md:p-8 rounded-lg md:w-3/4">
@@ -150,21 +179,12 @@ export default function Home() {
               />
             </div>
             {filteredCommands.map((command, index) => (
-              <div
-                key={index}
-                className={`command-card bg-[#191919] p-4 rounded-lg mb-4 ${
-                  activeCommand === index ? 'bg-[#191919]' : ''
-                }`}
-              >
-                <div className="command-header flex justify-between items-center">
-                  <div className="command-name text-[#8652ff] font-bold">{command.name}</div>
-                </div>
-                <div className="command-description text-white mt-2">{command.description}</div>
+              <div key={index} className="mb-4 bg-[#191919] p-4 rounded-lg mb-4">
+                <CommandCard command={command} />
               </div>
             ))}
           </div>
         </div>
       </main>
-    </client>
   );
 }
